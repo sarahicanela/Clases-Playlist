0e416e715a2b95d96b2acc446c1ff077a00a93dd
Sarahicanela669@gmail.com <>
2024-03-07 18:53:55 -0600

    Clases-Playlit

---
+++ src/main/java/com/mycompany/playlistt/Musicas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.playlistt;
+
+/**
+ *
+ * @author raymu
+ */
+public class Musicas {
+    
+    String title;
+    String album;
+    String date;
+    int time;
+
+
+   public Musicas(String title, String album, String date, int time ){
+       this.title=title;
+       this.album=album;
+       this.date=date;
+       this.time=time;
+
+   } 
+
+   public void ShowPlaylist(){
+       System.out.println("Title "+title);
+       System.out.println("Album "+album);
+       System.out.println("Date "+date);
+       System.out.println("Time "+time);
+   }
+
+}
---
+++ src/main/java/com/mycompany/playlistt/Playlistt.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlistt;
+
+/**
+ *
+ * @author raymu
+ */
+public class Playlistt {
+
+    public static void main(String[] args) {
+        Musicas p1 = new Musicas ("El paso del gigante", "Cumbias Mexicanas", "1853",3);
+        p1.ShowPlaylist();
+    }
+}
